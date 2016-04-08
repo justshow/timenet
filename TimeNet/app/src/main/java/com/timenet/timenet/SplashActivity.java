76132033ca9276673ae4eec867b3ab27a7532ca8
justshow <1173626116@qq.com>
2016-04-08 12:04:43 +0800

    建立项目

--- TimeNet/app/src/main/java/com/timenet/timenet/SplashActivity.java
+++ TimeNet/app/src/main/java/com/timenet/timenet/SplashActivity.java
@@ -10,7 +10,7 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        //djajpog a
         initView();
     }
 
