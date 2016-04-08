c6853ea5755f0b46b62e932c0c3564bf0b07d70a
justshow <1173626116@qq.com>
2016-04-08 14:09:55 +0800

    建立项目

--- TimeNet/app/src/main/java/com/timenet/timenet/SplashActivity.java
+++ TimeNet/app/src/main/java/com/timenet/timenet/SplashActivity.java
@@ -2,7 +2,9 @@ package com.timenet.timenet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class SplashActivity extends Activity {
     private Switch tb_button;
@@ -16,6 +18,11 @@ public class SplashActivity extends Activity {
 
     private void initView() {
         tb_button = (Switch)findViewById(R.id.tb_button);
-
+        tb_button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(SplashActivity.this,"点击了",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
